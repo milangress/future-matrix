45701b11de124397c9637668a0a66858d9967254
Milan G <dev@milangress.de>
2020-10-14 14:36:22 +0200

    abstract HorizontalAxis into component

---
+++ src/components/HorizontalAxis.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  a-entity(:rotation="rotation")
+    a-box(position='0 0 0' depth="0.1" height="0.1" width="20" color='#9e4cd9')
+    a-entity(position="11 0 0" v-bind:text="leftTextObject")
+    a-entity(position="-11 0 0" v-bind:text="rightTextObject")
+    a-entity(position="11 0 0" rotation='0 180 0' v-bind:text="leftBackTextObject")
+    a-entity(position="-11 0 0" rotation='0 180 0' v-bind:text="rightBackTextObject")
+</template>
+
+<script>
+
+const mainTextObject = {
+  width: 10,
+  wrapCount: '20',
+  color: 'black'
+}
+
+export default {
+  name: 'HorizontalAxis',
+  props: {
+    leftTxt: String,
+    rightTxt: String,
+    rotation: String
+  },
+  data () {
+    return {
+      leftTextObject: {
+        ...mainTextObject,
+        anchor: 'left',
+        align: 'left',
+        value: this.leftTxt
+      },
+      leftBackTextObject: {
+        ...mainTextObject,
+        anchor: 'right',
+        align: 'right',
+        value: this.leftTxt
+      },
+      rightTextObject: {
+        ...mainTextObject,
+        anchor: 'right',
+        align: 'right',
+        value: this.rightTxt
+      },
+      rightBackTextObject: {
+        ...mainTextObject,
+        anchor: 'left',
+        align: 'left',
+        value: this.rightTxt
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,8 @@
 <template lang="pug">
   .home
     a-scene
-      a-entity(rotation='0 0 0')
-        a-box(position='0 0 0' depth="20" height="0.1" width="0.1" color='#9e4cd9')
-        a-entity(position="11 0 0" text="anchor: left; align: left; width: 10; side: double; wrapCount: 20; color: black; value: Nature over Human;")
-        a-entity(position="-11 0 0" text="anchor: right; align: right; width: 10; side: double; wrapCount: 20; color: black; value: Human over Nature;")
-      a-entity(rotation='0 90 0')
-        a-box(position='0 0 0' depth="20" height="0.1" width="0.1" color='#9E4CD9')
-        a-entity(position="11 0 0" text="anchor: left; align: left; width: 10; side: double; wrapCount: 20; color: black; value: Offen;")
-        a-entity(position="-11 0 0" text="anchor: right; align: right; width: 10; side: double; wrapCount: 20; color: black; value: Geschlossen;")
+      horizontal-axis(leftTxt="Nature over Human" rightTxt="Human over Nature")
+      horizontal-axis(rotation="0 90 0" leftTxt="Offen" rightTxt="Geschlossen")
       a-entity(rotation='0 90 90')
         a-box(position='0 0 0' rotation='0 90 0' depth="20" height="0.1" width="0.1" color='#9E4CD9')
         a-entity(position="11 0 0" rotation='-45 0 -90' text="anchor: center; align: center; width: 10; side: double; wrapCount: 20; color: black; value: Touching;")
@@ -20,10 +14,12 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import HorizontalAxis from '@/components/HorizontalAxis'
 
 export default {
   name: 'Home',
   components: {
+    HorizontalAxis
   }
 }
 </script>
