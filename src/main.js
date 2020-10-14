bd4fd4cf8735b759a146b325aa7570bf51b81ecf
Milan G <dev@milangress.de>
2020-10-14 21:29:39 +0200

    turn off aframe-animation-component

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 require('aframe')
 require('aframe-orbit-controls')
 require('aframe-text-geometry-component')
-require('aframe-animation-component')
+// require('aframe-animation-component')
 
 Vue.config.productionTip = false
 
