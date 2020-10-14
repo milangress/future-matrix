d0f72403749686a538641e269680e62a870ac524
Milan G <dev@milangress.de>
2020-10-14 12:39:44 +0200

    Add basic packages

--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+require('aframe')
+require('aframe-orbit-controls')
+require('aframe-text-geometry-component')
+
 Vue.config.productionTip = false
+
+Vue.config.ignoredElements = [
+  'a-scene',
+  'a-entity',
+  'a-camera',
+  'a-box',
+  'a-plane'
+]
 
 new Vue({
   router,
