0a455924ae355dea7bb82d2a240d2af4550ae737
Milan G <dev@milangress.de>
2021-03-15 20:29:15 +0100

    reset

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 // import { query } from '@oarepo/vue-query-synchronizer'
+// reset
 
 const routes = [
   {
