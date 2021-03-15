1383398e9dcb7d4bdcc85bed4bf6c3388220287b
Milan G <dev@milangress.de>
2021-03-15 12:48:20 +0100

    ignore aframe elements

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
-const OptimizePlugin = require('optimize-plugin')
-
+// const OptimizePlugin = require('optimize-plugin')
 module.exports = {
-  /* configureWebpack: {
-    plugins: [
-      new OptimizePlugin()
-    ]
-  } */
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.compilerOptions = {
+          ...(options.compilerOptions || {}),
+          isCustomElement: tag => tag.startsWith('a-') // /^x-/.test(tag)
+        }
+        return options
+      })
+  },
   devServer: {
     disableHostCheck: true
   }
