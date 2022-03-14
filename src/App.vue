094a19c1b18a7a91736acbf7e8b31527a8910bdf
Milan G <dev@milangress.de>
2022-03-14 21:11:05 +0100

    add svg filter & font

--- src/App.vue
+++ src/App.vue
@@ -8,39 +8,65 @@
   </div>
 </template>
 
-<style lang="stylus">
-@font-face
+<style>
+@font-face {
   font-family: 'oracle';
-  src: url('/fonts/oraclegm-regularmono-webfont-webfont.woff2') format('woff2'),
-    url('/fonts/oraclegm-regularmono-webfont-webfont.woff') format('woff');
+  src: url("/fonts/oraclegm-regularmono-webfont-webfont.woff2") format('woff2'), url("/fonts/oraclegm-regularmono-webfont-webfont.woff") format('woff');
   font-weight: normal;
   font-style: normal;
-
-@font-face
+}
+@font-face {
   font-family: "ArmandGrotesk";
-  src: local("ArmandGrotesk"),
-    url('/fonts/ArmandGroteskTRIAL-Bold.otf') format("opentype");
-  font-weight: bold
-
-@font-face
+  src: local("ArmandGrotesk"), url("/fonts/ArmandGroteskTRIAL-Bold.otf") format("opentype");
+  font-weight: bold;
+}
+@font-face {
   font-family: "ArmandGrotesk";
-  src: local("ArmandGrotesk"),
-    url('/fonts/ArmandGroteskTRIAL-SemiLight.otf') format("opentype");
+  src: local("ArmandGrotesk"), url("/fonts/ArmandGroteskTRIAL-SemiLight.otf") format("opentype");
   font-weight: normal;
   font-stretch: 87.5% 112.5%;
-
-@font-face
+}
+@font-face {
   font-family: "Sneaky-Times";
-  src: url('/fonts/Sneaky-Times.woff2') format('woff2'),
-    url('/fonts/Sneaky-Times.woff') format('woff');
-  font-weight: normal
-
-body
-  margin 0
-#app
-  font-family oracle, Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  color #ffffff
-
+  src: url("/fonts/Sneaky-Times.woff2") format('woff2'), url("/fonts/Sneaky-Times.woff") format('woff');
+  font-weight: normal;
+}
+@font-face {
+  font-family: "SissiDisplay";
+  src: url("/fonts/SissiDisplay-Regular.woff") format('woff');
+  font-weight: normal;
+}
+body {
+  margin: 0;
+  filter: var(--grain)
+}
+#app {
+  font-family: oracle, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #fff;
+}
+:root {
+  --grain: url('data:image/svg+xml,\
+    <svg xmlns="http://www.w3.org/2000/svg">\
+      <filter id="grain"> \
+      <feTurbulence type="turbulence" baseFrequency="0.5" numOctaves="2" result="turbulence"/> \
+      <feDisplacementMap in2="turbulence" in="SourceGraphic" scale="1" xChannelSelector="R" yChannelSelector="G"/> \
+      </filter> \
+  </svg>#grain');
+  --grain-2: url('data:image/svg+xml,\
+    <svg xmlns="http://www.w3.org/2000/svg">\
+      <filter id="grain"> \
+      <feTurbulence type="turbulence" baseFrequency="0.5" numOctaves="2" result="turbulence"/> \
+      <feDisplacementMap in2="turbulence" in="SourceGraphic" scale="4" xChannelSelector="R" yChannelSelector="G"/> \
+      </filter> \
+  </svg>#grain');
+  --grain-3: url('data:image/svg+xml,\
+    <svg xmlns="http://www.w3.org/2000/svg">\
+      <filter id="grain"> \
+      <feTurbulence type="turbulence" baseFrequency="0.5" numOctaves="2" result="turbulence"/> \
+      <feDisplacementMap in2="turbulence" in="SourceGraphic" scale="15" xChannelSelector="R" yChannelSelector="G"/> \
+      </filter> \
+  </svg>#grain');
+}
 </style>
